9276b92ca7603fb3fc9f314108e1cd83aa9621f1
byche <arthur_bochard@hotmail.com>
2014-12-19 22:31:09 +0100

    ..

--- src/servlet/communication/AddTrainingPlanServlet.java
+++ src/servlet/communication/AddTrainingPlanServlet.java
@@ -4,16 +4,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.json.simple.JSONObject;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.urlfetch.HTTPRequest;
+import com.google.appengine.repackaged.com.google.api.client.http.HttpRequest;
 
 public class AddTrainingPlanServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
